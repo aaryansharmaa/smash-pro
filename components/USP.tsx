43f5bcc485445019dc439513c0e4869378933295
aaryansharmaa <aaryans018@gmail.com>
2024-11-15 21:26:13 +0530

    bug

--- components/USP.tsx
+++ components/USP.tsx
@@ -65,16 +65,6 @@ export default function USP() {
             <div className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
               Why <Cover>Smash Pro Arena?</Cover>
             </div>
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-              className="text-gray-400 max-w-2xl mx-auto"
-            >
-              Experience the perfect blend of technology and sport in our
-              state-of-the-art facility
-            </motion.p>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-20">
