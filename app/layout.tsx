3cce2afe47042491bba693eb8482181eff33b4d6
aaryansharmaa <aaryans018@gmail.com>
2024-11-20 13:54:22 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import JsonLd from "@/components/JsonLd";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -62,6 +63,7 @@ export default function RootLayout({
         {children}
         <JsonLd />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
